1e615232df0d7bba0bc15c76f8d5ecb6d81a1bf1
Farjana1khan <farjana09khan@gmail.com>
2025-01-09 15:21:30 +0530

    sign in enhanced

--- app/urls.py
+++ app/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('registration/', RegisterUserView.as_view()),
-    path('sign-in/', SignInView.as_view()),
-    path('log-in/', LogInView.as_view()),
-    path('otp_verify/', OTPVerificationView.as_view()),
-    path('profile/', UserProfileView.as_view()),
-    path('profile/update/', UserProfileView.as_view()),
-    path('text/create/', TextMessageView.as_view()),
-    path('text/list/', TextMessageView.as_view()),
-    path('push-notification/', PushNotificationView.as_view()),
-    path('notifications/', GetNotificationsView.as_view()),
+    path('registration', RegisterUserView.as_view()),
+    path('sign-in', SignInView.as_view()),
+    path('log-in', LogInView.as_view()),
+    path('otp_verify', OTPVerificationView.as_view()),
+    path('profile', UserProfileView.as_view()),
+    path('profile/update', UserProfileView.as_view()),
+    path('text/create', TextMessageView.as_view()),
+    path('text/list', TextMessageView.as_view()),
+    path('push-notification', PushNotificationView.as_view()),
+    path('notifications', GetNotificationsView.as_view()),
 
 ]
 
--- app/views.py
+++ app/views.py
@@ -12,18 +12,48 @@ import json, traceback
 class RegisterUserView(APIView):
     permission_classes = [AllowAny]
     def post(self, request):
-        accont_types = ('email','gmail')
+        account_types = ('email','gmail')
         try:
             request_data = request.data if isinstance(request.data,dict) else json.loads(request.data)
             name = request_data.get('name','').strip()
             email = request_data.get('email','').strip()
             password = request_data.get('password','').strip()
-            accont_type = request_data.get('accont_type','').strip()
+            account_type = request_data.get('account_type','').strip()
             if not all([email,password]):
                 raise ValueError('Email & Password required feilds.')
-            if accont_type not in accont_types:
-                raise ValueError(f'accont_type required field , It should be `{'/'.join(accont_types)}` only.')
-            user_obj = User.objects.filter(email=email,username=email) 
+            if account_type not in account_types:
+                raise ValueError(f'account_type required field , It should be `{'/'.join(account_types)}` only.')
+            user_obj = User.objects.filter(email=email,username=email)
+
+            if account_type == 'gmail':
+                if user_obj.filter(is_active=True,is_verified=True).exists():
+                    user = user_obj.last()
+                elif user_obj.filter(is_active=False,is_verified=False).exists():
+                    user = user_obj.last()
+                    user.is_active =True
+                    user.is_verified =True
+                    user.save()
+                else:
+                    # misc data
+                    request_data.pop("password")
+                    request_data.pop("email")
+                    request_data.pop("name")
+                    misc = request_data
+
+                    user = User.objects.create_user(name=name,
+                                                    email=email,
+                                                    username=email,
+                                                    password=password,
+                                                    is_active=True,
+                                                    is_verified=True,
+                                                    misc=misc)
+                    UserProfile.objects.create(user=user)
+
+                    refresh = RefreshToken.for_user(user)
+                    return Response({
+                        'token': str(refresh.access_token),
+                        'message': 'Email verified.','status':200}, status=status.HTTP_200_OK)
+
             if user_obj.filter(is_active=True,is_verified=True).exists():
                 return Response({'message': 'Email already exists.','status':400}, status=status.HTTP_400_BAD_REQUEST)
             elif user_obj.filter(is_active=False,is_verified=False).exists():
@@ -39,7 +69,7 @@ class RegisterUserView(APIView):
                                                 email=email,
                                                 username=email,
                                                 password=password,
-                                                is_active=False,
+                                                is_active=True,
                                                 is_verified=False,
                                                 misc=misc)
                 UserProfile.objects.create(user=user)
@@ -117,8 +147,7 @@ class LogInView(APIView):
                                 status=status.HTTP_401_UNAUTHORIZED)
                 
             if not all([user.is_verified, user.is_active]):
-                    return Response({'message': 'Email not verified!','status':403},
-                        status=status.HTTP_403_FORBIDDEN)
+                    return Response({'message': f'OTP sent on email `{email}`, please verify.','status':201}, status=status.HTTP_201_CREATED)
 
             refresh = RefreshToken.for_user(user)
             
